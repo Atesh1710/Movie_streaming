a33db6cb356b51e6914822fbf4be3d6e91c2e2ee
Atesh <73840304+Atesh1710@users.noreply.github.com>
2024-04-15 12:50:52 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,37 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What genres well perfer?",
+    options: [
+      "Action",
+      "Crime",
+      "Horror",
+      "Comedies",
+      "Documentary"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What sub-genres well perfer?",
+    
+    options: [
+      "Crime Thriller",
+      "Disaster Thriller",
+      "Psychological Thriller",
+      "Techno Thriller"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Select the language",
+   
+    options: [
+      "Hindi",
+      "English",
+      "Tamil",
+      "Japanese"
+    ]
+  },
+    ,
+];
